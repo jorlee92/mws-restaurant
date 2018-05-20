27b1f7ee2673dd2ca38178b6d386111c835d03a8
jorlee92 <jorlee92@gmail.com>
2018-05-20 15:04:38 -0700

    feat: Responsiveness improvements and added service worker functionality.
    Changed CSS so that it works as the background to the details page on
    smaller screens, configured a service worker to cache requests.

---
+++ sw.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = 'uda-reviews-v1';
+var urlsToCache = [
+  '/',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => console.log("Cached files"))
+  );
+});
+
+//See https://developers.google.com/web/fundamentals/primers/service-workers/
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+  
+          let fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
